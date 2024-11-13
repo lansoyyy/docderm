eec1323d7c47571b7d4a7d86215d76e231ce61bb
Lans12345 <olanalans12345@gmail.com>
2024-11-14 07:02:53 +0800

    updated data

--- lib/utils/data.dart
+++ lib/utils/data.dart
@@ -88,4 +88,82 @@ List<SkinDisease> skinDiseases = [
     treatment: 'Cryotherapy, topical medications, or laser therapy.',
     prevention: 'Limit sun exposure and use sunscreen regularly.',
   ),
+  SkinDisease(
+    acronym: 'VIT',
+    name: 'Vitiligo',
+    description:
+        'A condition in which the skin loses melanin, leading to white patches on the skin.',
+    treatment:
+        'Topical steroids, light therapy, and, in some cases, surgery to restore skin color.',
+    prevention:
+        'No definitive prevention, but avoiding skin trauma and stress may help manage the condition.',
+  ),
+  SkinDisease(
+    acronym: 'ROS',
+    name: 'Rosacea',
+    description:
+        'A chronic skin condition that causes redness and visible blood vessels on the face.',
+    treatment:
+        'Topical or oral medications, laser therapy, and lifestyle changes to avoid triggers.',
+    prevention:
+        'Avoiding known triggers such as sun exposure, spicy foods, and stress can help manage symptoms.',
+  ),
+  SkinDisease(
+    acronym: 'PSO',
+    name: 'Psoriasis',
+    description:
+        'An autoimmune condition that causes rapid skin cell buildup, leading to scaly patches on the skin.',
+    treatment:
+        'Topical treatments, phototherapy, oral or injectable medications.',
+    prevention:
+        'No definitive prevention, but avoiding skin injuries and managing stress may help.',
+  ),
+  SkinDisease(
+    acronym: 'HIV',
+    name: 'Hives',
+    description:
+        'Raised, itchy welts on the skin that are often triggered by an allergic reaction.',
+    treatment:
+        'Antihistamines, avoiding allergens, and applying soothing lotions.',
+    prevention:
+        'Avoid known allergens, stress management, and wearing loose clothing.',
+  ),
+  SkinDisease(
+    acronym: 'CPX',
+    name: 'Chicken Pox',
+    description:
+        'A highly contagious viral infection that causes an itchy rash and flu-like symptoms.',
+    treatment:
+        'Antihistamines, calamine lotion, and antiviral medications in severe cases.',
+    prevention: 'Vaccination is the most effective way to prevent chickenpox.',
+  ),
+  SkinDisease(
+    acronym: 'AA',
+    name: 'Alopecia Areata',
+    description:
+        'An autoimmune disorder that causes hair loss on the scalp and other parts of the body.',
+    treatment:
+        'Topical corticosteroids, immunotherapy, and, in some cases, hair transplants.',
+    prevention:
+        'No definitive prevention; stress management may help in some cases.',
+  ),
+  SkinDisease(
+    acronym: 'ECZ',
+    name: 'Eczema',
+    description:
+        'A condition that causes inflamed, itchy, cracked, and rough skin.',
+    treatment:
+        'Moisturizers, corticosteroid creams, and avoiding triggers like harsh soaps.',
+    prevention:
+        'Avoiding known irritants, maintaining skin hydration, and stress management.',
+  ),
+  SkinDisease(
+    acronym: 'ACN',
+    name: 'Acne',
+    description:
+        'A skin condition that occurs when hair follicles become clogged with oil and dead skin cells, causing pimples and inflammation.',
+    treatment: 'Topical creams, oral medications, and lifestyle changes.',
+    prevention:
+        'Regular skin cleansing, avoiding heavy makeup, and managing diet and stress.',
+  ),
 ];
