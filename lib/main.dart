17e3f0f61f710f085c000c6261e61b853de00c8b
Lans12345 <olanalans12345@gmail.com>
2024-12-05 16:50:11 +0800

    fixed

--- lib/main.dart
+++ lib/main.dart
@@ -34,7 +34,7 @@ class MyApp extends StatelessWidget {
             if (snapshot.hasData) {
               return const HomeScreen();
             } else {
-              return const AdminScreen();
+              return const LandingScreen();
             }
           }),
     );
